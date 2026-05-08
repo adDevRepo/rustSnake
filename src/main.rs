92d698aa9f003ee316c2e63d9b8364510b5999ff
adrienpineau <pineau.adrien@hotmail.com>
2026-05-08 17:09:28 +0200

    correction main

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
-use macroquad::prelude::*;
+mod direction;
+mod game;
+mod snake;
+mod apple;
+
 
 #[macroquad::main("Snake")]
 async fn main() {
-    loop {
-        clear_background(BLACK);
-        draw_circle(100.0, 100.0, 10.0, GREEN);
 
-        draw_text("Snake Game", 300.0, 40.0, 40.0, YELLOW);
-
-        next_frame().await;
-    }
 }
